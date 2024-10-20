35277b4d8ef205615a58a52c039c96e430d4ddeb
kurt rodriguez <kurt.rodriguez@clave.cl>
2024-10-19 19:37:44 -0500

    se agrega url cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['https://react-manage-projects.vercel.app'], 
+  origin: 'https://merng-client-seven.vercel.app/', 
   credentials: true,
   methods: ["POST", "GET", "DELETE", "UPDATE"]
 }));
