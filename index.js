f481eeb33622a793922044371e28de1657df0dd3
kurt rodriguez <kurt.rodriguez@clave.cl>
2024-10-19 19:39:17 -0500

    se corrige url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: 'https://merng-client-seven.vercel.app/', 
+  origin: 'https://merng-client-seven.vercel.app/graphql', 
   credentials: true,
   methods: ["POST", "GET", "DELETE", "UPDATE"]
 }));
